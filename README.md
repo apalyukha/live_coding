ea6b69cdb4368996ab49108b22063fa843d31e89
apalyukha <apalyukha@gmail.com>
2024-06-06 15:32:09 +0300

    refactoring code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # LiveCoding
+## Run test
+1. `pytest -v -s`
+2. `python -m pytest --alluredir allure-results`
+3. `allure serve allure-results`
+
+## Pre Commit
+1. `pre-commit autoupdate`
+2. `pre-commit run --all-files`
--- conftest.py
+++ conftest.py
@@ -1,18 +1,17 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+    # options = Options()
+    # options.add_argument("--headless")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--window-size=1920,1080")
+    # driver = webdriver.Chrome(options=options)
 
-    # driver = webdriver.Chrome()
+    driver = webdriver.Chrome()
     request.cls.driver = driver
     yield driver
     driver.quit()
